4170967d6464dfc289701f663edabcac33c94e45
Marta Jopp <martapeterson@gmail.com>
2018-02-16 15:42:46 -0600

    inline apidocs

--- server/routes/product.router.js
+++ server/routes/product.router.js
@@ -19,11 +19,34 @@ var Product = mongoose.model('Product', ProductSchema, 'products');
 const axios = require("axios");
 let keyword;
 
-router.get('/:id', function (req, res) {  
+/**
+ * @api {get} /products/:id Find the product
+ * @apiGroup Products
+ * @apiParam {id} id product_id
+* @apiSuccessExample Success-Response:
+* HTTP/1.1 201 OK
+ * @apiSuccessExample {json} Success
+ *    HTTP/1.1 200 OK
+ *    [{
+ *      "product_id": 12345678
+ *      "name": "pizza",
+ *      "current_price": {
+ *          "currency_code": "USD"
+ *          "value": 6.99
+ *      }
+ *    }]
+*
+ * @apiErrorExample {json} product not found
+ *    HTTP/1.1 404 Not Found
+ * @apiErrorExample {json} Find error
+ *    HTTP/1.1 500 Internal Server Error
+ */
+
+router.get('/:id', function (req, res) {
     let keyword = req.params.id;
     keyword = keyword.toString();
     const url =
-        "http://redsky.target.com/v2/pdp/tcin/" 
+        "http://redsky.target.com/v2/pdp/tcin/"
         + keyword + "?excludes=taxonomy,price,promotion,bulk_ship,"
         + "rating_and_review_reviews,rating_and_review_statistics,"
         + "question_answer_statistics";
@@ -36,12 +59,12 @@ router.get('/:id', function (req, res) {
             // var query = {}; 
             // query['product_id'] = { "$eq": 1669652 }
             keyword = Number(keyword)
-            Product.findOne({"product_id": keyword}, function (err, productFound) {
+            Product.findOne({ "product_id": keyword }, function (err, productFound) {
                 if (err) {
                     console.log("Error!", err);
                     res.sendStatus(500);
                 } else {
-                    
+
                     var sendProduct = {
                         name: sendBack,
                         product_id: productFound.product_id,
@@ -51,38 +74,57 @@ router.get('/:id', function (req, res) {
                         }
 
                     }
-                            
+
                     console.log('send back', sendProduct);
                     res.send(sendProduct)
                 }
-            });            
+            });
             // res.send(sendBack)
         } catch (error) {
             console.log('ERRORRRRRRRRRRRR');
             res.sendStatus(404)
         }
     };
-   getProduct(url)
+    getProduct(url)
 })
 
+/**
+ * @api {put} /products/:id Update a product price
+ * @apiGroup Products
+ * @apiParam {id} id product_id
+ * @apiParam {String} currency_code Products new currency code 
+ * @apiParam {Number} value Products new value
+ * @apiParamExample {json} Input
+ *    {
+ *      "currency_code": "USD",
+ *      "value": 109.99
+ *    }
+ * @apiSuccessExample {json} Success
+ *    HTTP/1.1 204 No Content
+ * @apiErrorExample {json} Update error
+ *    HTTP/1.1 500 Internal Server Error
+ */
+
 //update the price of the product
-router.put('/:id', function (req, res) {  
-let keyword = req.params.id;
-    Product.update({ "product_id": keyword }, {$set: {
-        "current_price": {
-            "currency_code": req.body.currency_code,
-            "value": req.body.value
+router.put('/:id', function (req, res) {
+    let keyword = req.params.id;
+    Product.update({ "product_id": keyword }, {
+        $set: {
+            "current_price": {
+                "currency_code": req.body.currency_code,
+                "value": req.body.value
+            }
         }
-    }}
+    }
         , function (err, productFound) {
-        if (err) {
-            console.log("Error!", err);
-            res.sendStatus(500);
-        } else {
-            console.log('happened')
-            res.sendStatus(201)
-        };   
-    }) 
+            if (err) {
+                console.log("Error!", err);
+                res.sendStatus(500);
+            } else {
+                console.log('happened')
+                res.sendStatus(201)
+            };
+        })
 })//end update route
 
 //Router available to other files
