2a1e5242b422fd6242d3bded82674c234a87dc09
Marta Jopp <martapeterson@gmail.com>
2018-02-16 20:45:38 -0600

    added tests

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -5,23 +5,34 @@ var app = require('../server.js'); //
 var expect = chai.expect;
 chai.use(chaiHttp);
 
-// describe('My server tests'), function() {
-// var app;
-// done();
-before(function(done){
-
-    done();
-});
-describe('Get Product Route', function (){
-    it('should return JSON when passed "1386042"', function (done) {
+//get route integrated testing
+describe('Get Product Route', function () {
+    it('should return JSON when passed "13860428"', function (done) {
         chai.request(app)
             .get('/products/13860428')
-        .end(function(err, response){
-            expect(response).to.be.json;
-            expect(response).to.have.status(200);
-            done();
-        })
+            .end(function (err, response) {
+                expect(response).to.be.json;
+                expect(response).to.have.status(200);
+                done()
+            })
     }
     )
 })
-// }
+
+//put route integrated testing
+describe('Update Product Route', function () {
+    it('should respond with a 204 when existing product is updated"', function (done) {
+        chai.request(app)
+            .put('/products/13860428')
+            .send({
+                current_price: {
+                    currency_code: 'USD',
+                    value: '110.99'
+                }
+            })
+            .end(function (err, response) {
+                expect(response).to.have.status(204)
+                done()
+            })
+    })
+})
